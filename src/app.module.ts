6c279a79fe7eb8698ef5127e19bb397b50bbf33a
Houdini <nonsoamadi@aol.com>
2020-05-27 01:17:06 +0100

    setup mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './books/book.module';
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule } from '@nestjs/config'
+
 
 @Module({
-  imports: [],
+  imports: [BookModule, ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
